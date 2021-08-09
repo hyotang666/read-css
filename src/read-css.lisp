1a58857528b18e98101b08b3a0a7aceb1fdd5e7a
SATO Shinichi <hyotang.karakoma@gmail.com>
2021-08-09 18:30:04 +0900

    Fix: CONSTANTS to wrap with EVAL-WHEN due to compiler claims.

--- src/read-css.lisp
+++ src/read-css.lisp
@@ -22,35 +22,33 @@
   ;; https://www.w3.org/TR/css-syntax-3/#non-ascii-code-point
   (<= #x80 (char-code char)))
 
+
+
 ;;;; CONSTANTS
-;; https://www.w3.org/TR/css-syntax-3/#digit
 
-(unless (boundp '+digits+)
-  (defconstant +digits+ "1234567890"))
-
-;; https://www.w3.org/TR/css-syntax-3/#uppercase-letter
-
-(unless (boundp '+uppercase-letters+)
-  (defconstant +uppercase-letters+
-    (coerce
-      (loop :for code :upfrom (char-code #\A) :to (char-code #\Z)
-            :collect (code-char code))
-      'string)))
-
-;; https://www.w3.org/TR/css-syntax-3/#lowercase-letter
-
-(unless (boundp '+lowercase-letters+)
-  (defconstant +lowercase-letters+
-    (coerce
-      (loop :for code :upfrom (char-code #\a) :to (char-code #\z)
-            :collect (code-char code))
-      'string)))
-
-;; https://www.w3.org/TR/css-syntax-3/#letter
-
-(unless (boundp '+letters+)
-  (defconstant +letters+
-    (concatenate 'string +uppercase-letters+ +lowercase-letters+)))
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  ;; Predicates below needs this eval-when.
+  ;; https://www.w3.org/TR/css-syntax-3/#digit
+  (unless (boundp '+digits+)
+    (defconstant +digits+ "1234567890"))
+  ;; https://www.w3.org/TR/css-syntax-3/#uppercase-letter
+  (unless (boundp '+uppercase-letters+)
+    (defconstant +uppercase-letters+
+      (coerce
+        (loop :for code :upfrom (char-code #\A) :to (char-code #\Z)
+              :collect (code-char code))
+        'string)))
+  ;; https://www.w3.org/TR/css-syntax-3/#lowercase-letter
+  (unless (boundp '+lowercase-letters+)
+    (defconstant +lowercase-letters+
+      (coerce
+        (loop :for code :upfrom (char-code #\a) :to (char-code #\z)
+              :collect (code-char code))
+        'string)))
+  ;; https://www.w3.org/TR/css-syntax-3/#letter
+  (unless (boundp '+letters+)
+    (defconstant +letters+
+      (concatenate 'string +uppercase-letters+ +lowercase-letters+))))
 
 ;;;; PREDICATES
 
